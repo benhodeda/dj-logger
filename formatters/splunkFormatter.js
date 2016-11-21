54617b8a0c74078817fe68261a9064fb4f343b99
Ben <benhodeda@gmail.com>
2016-11-21 10:29:43 +0200

    winston deep clone logic change Error type of meta to simple Object type. remove logic depends of Error type from splunk formatter

--- formatters/splunkFormatter.js
+++ formatters/splunkFormatter.js
@@ -7,20 +7,12 @@ module.exports = class SplunkFormatter extends Formatter {
         super.format(log);
         let logString = `timestamp="${moment().format("x")}" severity="${log.level.toUpperCase()}" action="${log.message}"`;
 
-        if (log.meta instanceof Error) {
-            logString += ` innerException="${log.meta.message}"`
-        }
-
         _.forOwn(log.meta, function (value, key) {
             if (value && value.constructor !== Function) {
                 value = typeof(value) === "object" ? JSON.stringify(value) : value;
                 logString += ` ${key}="${value}"`;
             }
         });
-
-        if (log.meta instanceof Error) {
-            logString += ` stacktrace="${log.meta.stack.split('\n').join('\r\n')}"`
-        }
 
         return logString;
     }
